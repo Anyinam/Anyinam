71987f1555674800ccdddd515c4ee6416dba2bed
Anyinam <83980404+Anyinam@users.noreply.github.com>
2023-02-21 16:15:13 +0100

    Add files via upload

---
+++ Personal Tutorial/example.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from Ipython.display import clear_output
+from six.moves import urllib
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+d_train=pd.read_csv(r"D:\THESIS\th\First.csv")
